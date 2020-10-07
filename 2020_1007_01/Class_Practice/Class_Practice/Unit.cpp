4414f41cc751c7fe793d064f5a3e3c0ac22bd807
ono-hikaru81 <Onohikaru0801@gmail.com>
2020-10-07 15:16:38 +0900

    リファクタリング作業その２ ・Unitクラスの変数をPrivateに変更 ・ステータスを画面に表示するための関数を作成 ・main関数で直接ステータスを表示していた部分を、関数呼び出しに変更

--- 2020_1007_01/Class_Practice/Class_Practice/Unit.cpp
+++ 2020_1007_01/Class_Practice/Class_Practice/Unit.cpp
@@ -3,21 +3,8 @@
 
 int main()
 {
-	
 	Unit unit;
-
-	printf("================\n");
-	printf("パラメーター\n");
-	printf("================\n");
-	printf("m_hp  = 0\n", unit.m_hp);
-	printf("m_str = 0\n", unit.m_str);
-	printf("m_mag = 0\n", unit.m_mag);
-	printf("m_tec = 0\n", unit.m_tec);
-	printf("m_spd = 0\n", unit.m_spd);
-	printf("m_def = 0\n", unit.m_def);
-	printf("m_mde = 0\n", unit.m_mde);
-	printf("m_luc = 0\n", unit.m_luc);
-	printf("================\n");
+	unit.PrintStatus();
 
 	system("pause");
 	return 0;
--- 2020_1007_01/Class_Practice/Class_Practice/Unit.h
+++ 2020_1007_01/Class_Practice/Class_Practice/Unit.h
@@ -11,7 +11,26 @@ public:
 	// デストラクター
 	~Unit();
 
-	int m_hp;  // 体力
+	void PrintStatus()
+	{
+		Unit unit;
+
+		printf("================\n");
+		printf("パラメーター\n");
+		printf("================\n");
+		printf("m_hp  = 0\n", unit.m_hp);
+		printf("m_str = 0\n", unit.m_str);
+		printf("m_mag = 0\n", unit.m_mag);
+		printf("m_tec = 0\n", unit.m_tec);
+		printf("m_spd = 0\n", unit.m_spd);
+		printf("m_def = 0\n", unit.m_def);
+		printf("m_mde = 0\n", unit.m_mde);
+		printf("m_luc = 0\n", unit.m_luc);
+		printf("================\n");
+	}
+
+private:
+    int m_hp;  // 体力
 	int m_str; // 力
 	int m_mag; // 魔力
 	int m_tec; // テク
