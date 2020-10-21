1038627741b222f8ec296120b19e8ef84e39daef
ono-hikaru81 <Onohikaru0801@gmail.com>
2020-10-21 14:04:31 +0900

    派生クラスのアドレスを、基底クラスのポインタ変数へ代入することができることを確認する

---
+++ 2020_1021_03/Practice03/Practice03_02/Enemy.cpp
@@ -0,0 +1,56 @@
+#include "Enemy.h"
+
+Enemy::Enemy()
+{
+	m_Routine = 0;
+}
+
+Enemy::~Enemy()
+{
+
+}
+
+void Enemy::Exex()
+{
+
+}
+
+void Enemy::Draw()
+{
+
+}
+
+//void Enemy::SetHp(int hp)
+//{
+//	m_Hp = hp;
+//}
+//
+//void Enemy::SetPos(float x, float y)
+//{
+//	m_PosX = x, m_PosY = y;
+//}
+//
+//void Enemy::SetMoveSpeed(float speed)
+//{
+//	m_MoveSpeed = speed;
+//}
+//
+//int Enemy::GetHp()
+//{
+//	return m_Hp;
+//}
+//
+//float Enemy::GetPosX()
+//{
+//	return m_PosX;
+//}
+//
+//float Enemy::GetPosY()
+//{
+//	return m_PosY;
+//}
+//
+//float Enemy::GetMoveSpeed()
+//{
+//	return m_MoveSpeed;
+//}
---
+++ 2020_1021_03/Practice03/Practice03_02/Enemy.h
@@ -0,0 +1,42 @@
+#ifndef ENEMY_H
+#define ENMEY_H
+
+#include"Base.h"
+
+//===========================
+//エネミーークラス
+//===========================
+class Enemy:public Base
+{
+public:
+	Enemy();
+	~Enemy();
+
+public:
+	void Exex();
+	void Draw();
+
+private:
+	// 行動管理用の変数
+	int m_Routine;
+
+//public:
+//	void SetHp(int hp);
+//	void SetPos(float x, float y);
+//	void SetMoveSpeed(float speed);
+//
+//	int GetHp();
+//	float GetPosX();
+//	float GetPosY();
+//	float GetMoveSpeed();
+//
+//private:
+//	int m_Hp;
+//	int m_PosX;
+//	int m_PosY;
+//	int m_MoveSpeed;
+};
+
+
+
+#endif
