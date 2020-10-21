8d271046f2ad09702cd2145b06bd8555acfbb0d6
ono-hikaru81 <Onohikaru0801@gmail.com>
2020-10-21 13:34:31 +0900

    第３回その１、継承と汎化についての実習用プロジェクトを作成

---
+++ 2020_1021_03/Practice03/Practice03_01/Player.cpp
@@ -0,0 +1,56 @@
+#include "Player.h"
+
+Player::Player()
+{
+
+}
+
+Player::~Player()
+{
+
+}
+
+void Player::Exex()
+{
+
+}
+
+void Player::Draw()
+{
+
+}
+
+//void Player::SetHp(int hp)
+//{
+//	m_Hp = hp;
+//}
+//
+//void Player::SetPos(float x, float y)
+//{
+//	m_PosX = x, m_PosY = y;
+//}
+//
+//void Player::SetMoveSpeed(float speed)
+//{
+//	m_MoveSpeed = speed;
+//}
+//
+//int Player::GetHp()
+//{
+//	return m_Hp;
+//}
+//
+//float Player::GetPosX()
+//{
+//	return m_PosX;
+//}
+//
+//float Player::GetPosY()
+//{
+//	return m_PosY;
+//}
+//
+//float Player::GetMoveSpeed()
+//{
+//	return m_MoveSpeed;
+//}
---
+++ 2020_1021_03/Practice03/Practice03_01/Player.h
@@ -0,0 +1,40 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include"Base.h"
+
+//===========================
+//プレイヤークラス
+//===========================
+// 基底クラス(Base)を継承する
+// 派生クラス名：public(or private) 基底クラス名
+class Player:public Base
+{
+public:
+	Player();
+	~Player();
+
+public:
+	void Exex();
+	void Draw();
+
+//public:
+//	void SetHp(int hp);
+//	void SetPos(float x,float y);
+//	void SetMoveSpeed(float speed);
+//
+//	int GetHp();
+//	float GetPosX();
+//	float GetPosY();
+//	float GetMoveSpeed();
+//
+//private:
+//	int m_Hp;
+//	float m_PosX;
+//	float m_PosY;
+//	float m_MoveSpeed;
+};
+
+
+
+#endif
