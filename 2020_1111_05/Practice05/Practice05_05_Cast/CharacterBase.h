9c1a95374e059e402931cb2dbf87657e5c1d4eca
ono-hikaru81 <Onohikaru0801@gmail.com>
2020-11-11 17:40:08 +0900

    第５回実習その５
    C++的キャスト４種類の実践(未完成)

---
+++ 2020_1111_05/Practice05/Practice05_05_Cast/CharacterBase.h
@@ -0,0 +1,24 @@
+#ifndef CHARACTERBASE_H
+#define CHARACTERBASE_H
+
+class CharacterBase
+{
+public:
+	CharacterBase();
+	virtual ~CharacterBase();
+
+	void SetHp(int hp) { m_Hp = hp; }
+	
+	
+	void SetMp(int mp) { m_Mp = mp; }
+
+	int GetHp() const { return m_Hp; }
+
+	int GetMp() const { return m_Mp; }
+
+protected:
+	int m_Hp;
+	int m_Mp;
+
+};
+#endif
---
+++ 2020_1111_05/Practice05/Practice05_05_Cast/Enemy01.cpp
@@ -0,0 +1,12 @@
+#include "Enemy00.h"
+#include "Enemy01.h"
+
+Enemy01::Enemy01()
+{
+	m_Variation = 0;
+	m_Routine = 0;
+}
+
+Enemy01::~Enemy01()
+{
+}
