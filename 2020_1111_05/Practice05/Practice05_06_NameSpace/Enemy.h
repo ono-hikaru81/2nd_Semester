738523f30921ef9000a6dac3de8abcc8388e77a6
ono-hikaru81 <Onohikaru0801@gmail.com>
2020-11-11 18:03:16 +0900

    第５実習その６
    名前空間の導入

---
+++ 2020_1111_05/Practice05/Practice05_06_NameSpace/Enemy.h
@@ -0,0 +1,17 @@
+#ifndef ENEMY_H
+#define ENEMY_H
+
+// namespace 名前空間の書き方
+// namespace namesoaceの名前
+// {
+//		// グローバル変数やクラス定義など
+// }
+
+// Enemy名前空間
+namespace Enemy
+{
+	static const int Max = 10; // 敵最大数
+}
+
+
+#endif
---
+++ 2020_1111_05/Practice05/Practice05_06_NameSpace/Shot.h
@@ -0,0 +1,13 @@
+#ifndef SHOT_H
+#define SHOT_H
+
+namespace Shot
+{
+	static const int Max = 100; // ’eÅ‘å”
+}
+
+
+
+
+
+#endif
