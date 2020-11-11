ef355122612ab6dffb1414fe96383514e736ed04
ono-hikaru81 <Onohikaru0801@gmail.com>
2020-11-11 14:56:39 +0900

    第５回実習その３
    クラスと組み合わせた参照の使い方

---
+++ 2020_1111_05/Practice05/Practice05_03_ClassReference/CharacterBase.cpp
@@ -0,0 +1,12 @@
+#include "CharacterBase.h"
+
+CharacterBase::CharacterBase()
+	: m_Hp(10)
+	,m_Mp(8)
+{
+
+}
+
+CharacterBase::~CharacterBase()
+{
+}
---
+++ 2020_1111_05/Practice05/Practice05_03_ClassReference/Enemy00.h
@@ -0,0 +1,11 @@
+#ifndef ENEMY00_H
+#define ENEMY00_H
+#include "CharacterBase.h"
+
+class Enemy00 : public CharacterBase
+{
+public:
+	Enemy00();
+	~Enemy00();
+};
+#endif
