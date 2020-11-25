30161ede5595c7b89b80cfef1cb6126ca62a7140
ono-hikaru81 <Onohikaru0801@gmail.com>
2020-11-25 15:38:49 +0900

    第七回実習その３
    Vector2Dに代入、算術、比較演算子のオーバーロードを実践してみる
    ただし、それぞれ１つずつ

---
+++ 2020_1125_07/Practice07/Practice07_03_Operator/Vector2D.cpp
@@ -0,0 +1,55 @@
+#include "Vector2D.h"
+#include <Math.h>
+
+Vector2D::Vector2D()
+	:m_X(0.0f)
+	,m_Y(0.0f)
+{
+}
+
+Vector2D::Vector2D(float x_, float y_)
+	:m_X(x_)
+	,m_Y(y_)
+{
+}
+
+float Vector2D::GetLength() const
+{
+	return float(sqrt(m_X * m_X) + (m_Y * m_Y));
+}
+
+Vector2D& Vector2D::operator=(const Vector2D& vec)
+{
+	this->m_X = vec.m_X;
+	this->m_Y = vec.m_Y;
+	return *this;
+}
+
+Vector2D& Vector2D::operator+=(const Vector2D& vec)
+{
+	this->m_X += vec.m_X;
+	this->m_Y += vec.m_Y;
+	return *this;
+}
+
+const Vector2D Vector2D::operator+(const Vector2D& vec)
+{
+	Vector2D ans;
+	ans.m_X = this->m_X + vec.m_X;
+	ans.m_Y = this->m_Y + vec.m_Y;
+
+	return ans;
+}
+
+const Vector2D Vector2D::operator+(float val)
+{
+	Vector2D ans;
+	ans.m_X = this->m_X + val;
+	ans.m_Y = this->m_Y + val;
+	return ans;
+}
+
+bool Vector2D::operator>(const Vector2D& vec) const
+{
+	return this->GetLength() > vec.GetLength();
+}
